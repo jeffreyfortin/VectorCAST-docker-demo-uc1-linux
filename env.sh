1f5ffd413122e48566db29f6a22dc55bae3f22ce
Jeffrey Fortin <jeffrey.fortin@outlook.com>
2021-07-08 13:15:08 -0700

    update

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@ function get_env()
     export base=$(pwd)
 
     export REQ_REPO3_DIR=./REPO3
-    export SOURCE_DIR=./src
+    export SOURCE_DIR=src
  
     export ENVIRO_SEARCH_LIST_L1_RXTX=${SOURCE_DIR}/L1_HAL_MODEL
 }
